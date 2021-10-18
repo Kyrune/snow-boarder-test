1642fbfd023a89ec0ab7aa377b97bf8995d8e080
Kyrune <jn-01@centurytel.net>
2021-10-18 18:14:55 -0500

    Restart the level if player's head hits ground

--- CrashDetector.cs
+++ CrashDetector.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
