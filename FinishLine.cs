2cc4fea3b13d6a32cd8d75e5139c3ec696782726
Kyrune <jn-01@centurytel.net>
2021-10-19 18:01:02 -0500

    Add effect to finish line

--- FinishLine.cs
+++ FinishLine.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float invokeDelay = 0.5f;
+    [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", invokeDelay);
         }
     }
