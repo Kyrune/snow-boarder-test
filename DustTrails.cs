3d16de1238885be57ab0e9b75d316227d4b5538f
Kyrune <jn-01@centurytel.net>
2021-10-20 12:55:21 -0500

    Remove Start/Update methods

--- DustTrails.cs
+++ DustTrails.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class DustTrails : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
