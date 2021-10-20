8d549e060d42bb83f8893008550a73497ba0c166
Kyrune <jn-01@centurytel.net>
2021-10-20 11:37:06 -0500

    Add SurfaceEffector

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
     Rigidbody2D rb2d; 
+    SurfaceEffector2D surfaceEffector2D;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
