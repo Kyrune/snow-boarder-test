7dcb52c9119030f969da069ee6e0605fbe1b8129
Kyrune <jn-01@centurytel.net>
2021-11-04 09:55:26 -0500

    Add boolean check for crash

--- CrashDetector.cs
+++ CrashDetector.cs
@@ -9,9 +9,12 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
 
+    bool hasCrashed = false;
+
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Ground")
+        if (other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadSceneBoop", invokeDelayBoop);        
